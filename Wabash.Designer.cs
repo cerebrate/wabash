6840afa3d66784c6874b4e5599e3b8a8ab4bb301
Alistair Young <avatar@arkane-systems.net>
2016-10-16 01:09:08 -0500

    Form layout/behavior basics.

--- Wabash.Designer.cs
+++ Wabash.Designer.cs
@@ -29,11 +29,91 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Wabash));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.iconMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.mniOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnsSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.iconMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.iconMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Wabash: starting...";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // logBox
+            // 
+            this.logBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logBox.Location = new System.Drawing.Point(12, 12);
+            this.logBox.Name = "logBox";
+            this.logBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.logBox.Size = new System.Drawing.Size(440, 173);
+            this.logBox.TabIndex = 0;
+            // 
+            // iconMenu
+            // 
+            this.iconMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniOpen,
+            this.mnsSeparator,
+            this.mniExit});
+            this.iconMenu.Name = "iconMenu";
+            this.iconMenu.Size = new System.Drawing.Size(104, 54);
+            // 
+            // mniOpen
+            // 
+            this.mniOpen.Name = "mniOpen";
+            this.mniOpen.Size = new System.Drawing.Size(103, 22);
+            this.mniOpen.Text = "&Open";
+            this.mniOpen.Click += new System.EventHandler(this.mniOpen_Click);
+            // 
+            // mnsSeparator
+            // 
+            this.mnsSeparator.Name = "mnsSeparator";
+            this.mnsSeparator.Size = new System.Drawing.Size(100, 6);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(103, 22);
+            this.mniExit.Text = "E&xit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // Wabash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(464, 201);
+            this.Controls.Add(this.logBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(480, 240);
+            this.MinimizeBox = false;
+            this.Name = "Wabash";
+            this.Text = "Wabash";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Wabash_FormClosing);
+            this.VisibleChanged += new System.EventHandler(this.Wabash_VisibleChanged);
+            this.iconMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ListBox logBox;
+        private System.Windows.Forms.ContextMenuStrip iconMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniOpen;
+        private System.Windows.Forms.ToolStripSeparator mnsSeparator;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
     }
 }
 
--- Wabash.cs
+++ Wabash.cs
@@ -1,20 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿#region header
+
+// Wabash - Wabash.cs
+// 
+// Alistair J. R. Young
+// Arkane Systems
+// 
+// Copyright Arkane Systems 2012-2016.  All rights reserved.
+// 
+// Created: 2016-10-15 8:45 PM
+
+#endregion
+
+#region using
+
+using System ;
+using System.Windows.Forms ;
+
+#endregion
 
 namespace Wabash
 {
     public partial class Wabash : Form
     {
-        public Wabash()
+        private bool allowClosing ;
+
+        public Wabash () { this.InitializeComponent () ; }
+
+        private void Wabash_FormClosing (object sender, FormClosingEventArgs e)
         {
-            InitializeComponent();
+            if (!this.allowClosing)
+            {
+                // Minimize (to tray) instead of closing form.
+                e.Cancel = true ;
+                this.Hide () ;
+            }
         }
+
+        private void notifyIcon_MouseDoubleClick (object sender, MouseEventArgs e) => this.Show () ;
+
+        private void mniOpen_Click (object sender, EventArgs e) => this.Show () ;
+
+        private void mniExit_Click (object sender, EventArgs e)
+        {
+            this.allowClosing = true ;
+            this.Close () ;
+        }
+
+        private void Wabash_VisibleChanged(object sender, EventArgs e) => this.mniOpen.Enabled = !this.Visible ;
     }
 }
